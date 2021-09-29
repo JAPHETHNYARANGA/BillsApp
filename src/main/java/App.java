fc593fb0db501d141a1636ce6815472e2ac98c3c
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-09-29 07:54:10 +0300

    Created original files

---
+++ src/main/java/App.java
@@ -0,0 +1,29 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        get("/login", (request, response) -> {
+            return new ModelAndView(new HashMap(), "login.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/register", (request, response) -> {
+            return new ModelAndView(new HashMap(), "register.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/home", (request, response) -> {
+            return new ModelAndView(new HashMap(), "home.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/favorite_photos", (request, response) -> {
+            return new ModelAndView(new HashMap(), "Notifications.hbs");
+        }, new HandlebarsTemplateEngine());
+
+    }
+}
