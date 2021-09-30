9b76ce0a6e217b342809aab21518d716797b65e7
Your Name <youremail@domain.com>
2021-09-30 12:52:32 +0300

    created bills class and added variables and the getters and setters and constructor

---
+++ src/main/java/Bills.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+
+public class Bills {
+    private String name;
+    private int amount;
+    private Date date;
+
+    public Bills(String name, int amount, Date date) {
+        this.name = name;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
